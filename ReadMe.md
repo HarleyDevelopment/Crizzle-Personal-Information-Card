5a85a11e5e045cd8a5119c5db11698c3e9e4984e
Mr-Crizzle <82172684+Mr-Crizzle@users.noreply.github.com>
2021-08-31 21:30:16 +0100

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+Thank you for downloading the Crizzle Information Card
+
+How to install
+Step 1 - Open the config.js file and fill out all of the fields in there
+Step 2 - To change the background, fav-icon and the profile picute just go to the folder called Images and rename all
+of your new assets to the ones you see there in the folder
+
+Here are the names of the files incase you forget or you have given them the wrong names
+Favicon name = Fav-Icon.png
+Profile Picture name = Profile-Picture.png
+Background name = Background.png
+Make sure your profile picture is 512x512 pixels and that it is a PNG file
+
+Made by: Mr Crizzle#2845
---
+++ config.js
@@ -0,0 +1,7 @@
+var SiteName = `Crizzle`; // The information card name
+var UserName = `Mr Crizzle#2845`; // Your username
+var AboutMe = `This is the crizzle information card. This costs 0 money = meaning this is a very 0 money free product. You can download this product off of my website for free. So download me or else you will get attacked by a spider!`; // Anything you want to put into the about me part
+//To change the background, fav-icon and the profile picute just go to the folder called Images and rename all of your new assets to the ones you see there in the folder
+//Favicon name = Fav-Icon.png
+//Profile Picture name = Profile-Picture.png
+//Backgroun name = Background.png
